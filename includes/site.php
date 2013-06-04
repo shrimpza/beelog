aea8c25ba6e72778996c2d10d88ce23fd83cd80d
Kenneth Watson <shrimpza@users.noreply.github.com>
2013-06-04 16:45:04 +0000

    Initial commit.

---
+++ includes/site.php
@@ -0,0 +1,116 @@
+<?php
+
+require_once(dirname(__FILE__) . '/config.php');
+require_once('smarty3/Smarty.class.php');
+require_once(dirname(__FILE__) . '/functions.php');
+require_once(dirname(__FILE__) . '/database.php');
+require_once(dirname(__FILE__) . '/plugins.php');
+
+class Site {
+
+    var $plugins = array();
+    var $activePlugin = '';
+    var $tplVars = array();
+
+    function Site() {
+        $this->incPath = dirname(__FILE__) . '/libs/';
+
+        DB::getInstance($GLOBALS['config']['database']);
+
+        if (!isset($_GET['module'])) {
+            $_GET['module'] = $GLOBALS['config']['plugins']['default'];
+        }
+
+        $this->activePlugin = $_GET['module'];
+
+        $this->loadPlugins();
+    }
+
+    function loadPlugins() {
+        foreach ($GLOBALS['config']['plugins']['enabled'] as $plugin) {
+            include_once($GLOBALS['config']['plugins']['directory'] . $plugin . '/plugin.php');
+            $newPlug = new $plugin($this);
+            $this->plugins[$plugin] = $newPlug;
+        }
+    }
+
+    function output() {
+        $pluginTitle = '';
+        $content = '';
+        $sideBlocks = array();
+        $pluginCSS = false;
+
+        if (file_exists($GLOBALS['config']['plugins']['directory'] . $this->activePlugin . '/plugin.css')) {
+            $pluginCSS = $this->activePlugin . '/plugin.css';
+        }
+
+        foreach ($this->plugins as $name => $plugin) {
+            if ($name == $this->activePlugin) {
+                if (($this->user->id > 0) && ($plugin->level <= $this->user->level) || ($plugin->level <= 0)) {
+                    if (method_exists($plugin, 'getContent')) {
+                        $content = $plugin->getContent();
+                    }
+                } else {
+                    $content = '<h1>DENIED!</h1>';
+                }
+                $pluginTitle = $plugin->name;
+            }
+
+            if (($this->user->id > 0) && ($plugin->level <= $this->user->level) || ($plugin->level <= 0)) {
+                if (method_exists($plugin, 'getSideBox')) {
+                    $newBlock = array('title' => $plugin->name,
+                        'content' => $plugin->getSideBox());
+                    $sideBlocks[] = $newBlock;
+                }
+            }
+        }
+
+        $smarty = new Smarty();
+        $smarty->setTemplateDir($GLOBALS['config']['templates']['theme_dir'] . '/' . $GLOBALS['config']['templates']['theme']);
+        $smarty->setCompileDir($GLOBALS['config']['templates']['compile_dir']);
+
+        $smarty->assign('pluginTitle', $pluginTitle);
+        $smarty->assign('content', $content);
+        $smarty->assign('sideBlocks', $sideBlocks);
+        $smarty->assign('pluginCSS', $pluginCSS);
+
+        $this->tplVars['title'] = $GLOBALS['config']['site']['title'];
+        $this->tplVars['site_url'] = $GLOBALS['config']['site']['url'];
+        $this->tplVars['theme'] = $GLOBALS['config']['templates']['theme'];
+        if (!empty(apiStats::$errors)) {
+            $this->tplVars['errors'] = objectToArray(apiStats::$errors);
+        }
+
+        foreach ($this->tplVars as $var => $val) {
+            $smarty->assign($var, $val);
+        }
+
+        if (isset($_GET['popup'])) {
+            $smarty->display('popup.html');
+        } else {
+            $smarty->display('index.html');
+        }
+    }
+
+    function outputJson() {
+        $content = '';
+
+        foreach ($this->plugins as $name => $plugin) {
+            if ($name == $this->activePlugin) {
+                if (($this->user->id > 0) && ($plugin->level <= $this->user->level) || ($plugin->level <= 0)) {
+                    if (method_exists($plugin, 'getContentJson')) {
+                        $content = $plugin->getContentJson();
+                    }
+                } else {
+                    $content = '{}';
+                }
+            }
+        }
+
+        header('Content-type: text/plain');
+        echo $content;
+    }
+
+}
+
+?>
---
+++ sql/install-db.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS `apikey` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `keyid` int(11) NOT NULL,
+  `vcode` varchar(256) NOT NULL,
+  `character_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `user_id` (`user_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `mineralprice` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `typeid` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `user_id` (`user_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `timezone` varchar(128) NOT NULL default 'GMT',
+  `theme` varchar(64) NOT NULL,
+  `proxy` varchar(256) NOT NULL,
+  `level` tinyint(4) NOT NULL,
+  `activetime` datetime NOT NULL,
+  `smallicons` tinyint(4) default '0',
+  `char_apikey_id` int(11) NOT NULL,
+  `corp_apikey_id` int(11) NOT NULL,
+  `session_id` varchar(128) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `username` (`username`,`password`),
+  KEY `session_id` (`session_id`)
+);
