828f61e6c620bbec13d1258c1a4fb2d7309c79f5
shrimp.za@gmail.com <shrimp.za@gmail.com@cf5cadc5-7ae8-d2f3-c3a4-453a2b5b41a3>
2013-06-16 21:06:28 +0000

    Add status management. Fix status table SQL. Improve some img tags.

--- trunk/plugins/priority/plugin.php
+++ trunk/plugins/priority/plugin.php
@@ -7,16 +7,16 @@
 
             $this->site->plugins['mainmenu']->addLink('config', 'Priorities', '?module=priority', 'priority');
         }
-        
+
         function getContent() {
             if (isset($_GET['edit'])) {
                 $priority = DB::getObject('priority', $_GET['edit']);
 
-            return $this->render('priority', array('p' => objectToArray($priority)));
+                return $this->render('priority', array('p' => objectToArray($priority)));
             } else if (isset($_GET['delete'])) {
                 $priority = DB::getObject('priority', $_GET['delete']);
                 $priority->delete();
-                
+
                 return $this->priorityList();
             } else if (isset($_POST['id'])) {
                 $priority = DB::getObject('priority', $_POST['id']);
@@ -25,18 +25,19 @@
                 $priority->display_order = trim($_POST['display_order']);
 
                 $priority->save();
-                
+
                 return $this->priorityList();
             } else {
                 return $this->priorityList();
             }
         }
 
-         function priorityList() {
+        function priorityList() {
             $priorities = objectToArray(DB::getObjects('priority', '', 'display_order desc'));
 
             return $this->render('priorities', array('priorities' => $priorities));
         }
+
     }
 
 ?>
