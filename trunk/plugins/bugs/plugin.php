a4d21e83b1e8369ca8b4b87b67e6424592534d1a
shrimp.za@gmail.com <shrimp.za@gmail.com@cf5cadc5-7ae8-d2f3-c3a4-453a2b5b41a3>
2013-06-17 11:06:13 +0000

    Create/Edit bugs progress. Add configuration option to customise terminology used for "bugs".

--- trunk/plugins/bugs/plugin.php
+++ trunk/plugins/bugs/plugin.php
@@ -6,13 +6,20 @@
         var $level = 0;
 
         function bugs($site) {
+            $this->name = $GLOBALS['config']['bugs']['desc_plural'];
+
             $this->Plugin($site);
 
-            $this->site->plugins['mainmenu']->addLink('main', 'Bugs List', '?module=bugs', 'bugs_list');
+            $this->site->plugins['mainmenu']->addLink('main', 'List ' . $this->name, '?module=bugs', 'bugs_list');
+            $this->site->plugins['mainmenu']->addLink('main', 'New ' . $GLOBALS['config']['bugs']['desc'], '?module=bugs&edit=0', 'bugs_new');
         }
 
         function getContent() {
-            return $this->bugList();
+            if (isset($_GET['edit'])) {
+                return $this->editBug($_GET['edit']);
+            } else {
+                return $this->bugList();
+            }
         }
 
         function bugList($projectId = 0) {
@@ -30,6 +37,28 @@
             }
 
             return $this->render('list', array('bugs' => objectToArray($bugs)));
+        }
+
+        function editBug($id = 0) {
+            $bug = DB::getObject('bug', $_GET['edit']);
+
+            $priority = DB::getObjects('priority', '', 'display_order desc');
+            $status = DB::getObjects('status', '', 'display_order');
+            $projects = DB::getObjects('project', '', 'name');
+
+            return $this->render('edit', array(
+                        'b' => objectToArray($bug),
+                        'priority' => objectToArray($priority),
+                        'status' => objectToArray($status),
+                        'projects' => objectToArray($projects)));
+        }
+
+        function getContentJson() {
+            if (isset($_GET['project'])) {
+                $project = DB::getObject('project', $_GET['project']);
+                $initiatives = $project->get_initiative_list();
+                return json_encode($initiatives);
+            }
         }
 
     }
